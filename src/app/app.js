48d4a35d024a4c6ea0c22b20185a320790b086a1
Gilly Ames <gilly.ames@gmail.com>
2015-06-16 10:30:07 +0100

    Routing to project detail by url working

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-angular.module('portfolio', ['ui.router', 'components', 'portfolioServices', 'home'])
+angular.module('portfolio', ['ui.router', 'components', 'portfolioServices', 'home','projectdetail'])
 
   .run(function ($state) {
     $state.go('home');
---
+++ src/app/project-detail/project-detail.js
@@ -0,0 +1,34 @@
+angular.module("projectdetail", [])
+
+  .config(function ($stateProvider, BASE_URL) {
+
+    $stateProvider.state('projectdetail', {
+      url: '/project-detail/:id',
+      templateUrl: 'project-detail/project-detail.html',
+      controller: 'ProjectDetailCtrl as projectdetail',
+      resolve: {
+        project: function ($http, $stateParams, portfolioService) {
+          return portfolioService.getProjects()
+            .then(function (res) {
+              var projects =  res.data;
+              for (var i = projects.length - 1; i >= 0; i--) {
+                var nextProject = projects[i];
+                if(nextProject.id === $stateParams.id){
+                  return nextProject;
+                }
+              }
+            });
+        }
+       }
+    });
+
+  })
+
+
+  .controller("ProjectDetailCtrl", function (project, BASE_URL) {
+    var projectdetail = this;
+
+    projectdetail.project = project;
+    console.log(project.id);
+    projectdetail.BASE_URL = BASE_URL;
+  });
