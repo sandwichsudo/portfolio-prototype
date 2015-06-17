d0f8bb1a55b74b3715a17ff06ad2d1a37022956b
Gilly Ames <gilly.ames@gmail.com>
2015-06-17 16:54:02 +0100

    Added other pages to navigation service from json.

--- src/common/services/portfolioServices.js
+++ src/common/services/portfolioServices.js
@@ -12,15 +12,23 @@ angular.module("portfolioServices", [])
   }) 
   .service("navService", function ($http, BASE_URL) {
     this.getNavObject = function () {
-      return $http.get(BASE_URL + "/data/projects.json").then(function(res) {
-      	   var navObject ={};
-
-      	   navObject.projects = res.data; 
-		   navObject.isNavOpen = false;
-		   return navObject;
-		  }, function(reason) {
-		  console.err("Could not get Nav object");
-		});
+      return $http.get(BASE_URL + "/data/projects.json")
+      .then(function(res) {
+          var navObject ={};
+          navObject.projects = res.data; 
+          return $http.get(BASE_URL + "/data/pages.json")
+            .then(function(res) {
+              navObject.pages = res.data; 
+              navObject.isNavOpen = false;
+              return navObject;
+    		    }, function(reason) {
+    		      console.err("Could not get Nav object");
+            });
+          }, 
+          function(reason) {
+            console.err("Could not get projects object");
+		      }
+      );
     };
   });
 
